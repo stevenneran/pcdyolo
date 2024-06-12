92b9a32ffbcd6b81d80ba87e3681866e8811845e
Steven Chris <121673178+stevenneran@users.noreply.github.com>
2024-06-12 14:11:23 +0000

    message

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,13 +16,13 @@ import cv2
 from ultralytics import YOLO
 
 # Fungsi untuk menjalankan model YOLO dan menampilkan hasilnya
-def run_yolo(image_path):
-    model = YOLO('best.pt')  # Ganti dengan model yang Anda gunakan
+def run_yolo(model, image_path):
+    model = YOLO(model)
     results = model.predict(image_path, save=False, imgsz=320, conf=0.5)
     return results
 
 # Judul aplikasi
-st.title('YOLO Object Detection Dashboard')
+st.title('Deteksi Jamur pada Roti dengan YOLOv8')
 
 # Upload gambar
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
@@ -33,13 +33,17 @@ if uploaded_file is not None:
         f.write(uploaded_file.getbuffer())
 
     # Menjalankan model YOLO pada gambar yang diupload
-    results = run_yolo("uploaded_image.jpg")
+    results_1 = run_yolo("best.pt", "uploaded_image.jpg")
+    results_2 = run_yolo("best2.pt", "uploaded_image.jpg")
 
     # Mengambil gambar yang dihasilkan dari hasil deteksi
-    detected_img = results[0].plot()  # Assuming results is a list of results
+    detected_img_1 = results_1[0].plot()
+    detected_img_2 = results_2[0].plot()
 
     # Mengubah gambar hasil deteksi ke format yang bisa ditampilkan oleh Streamlit
-    detected_img_pil = Image.fromarray(detected_img)
+    detected_img_pil_1 = Image.fromarray(detected_img_1)
+    detected_img_pil_2 = Image.fromarray(detected_img_2)
 
     # Menampilkan hasil deteksi
-    st.image(detected_img_pil, caption='Detected Image', use_column_width=True)
+    st.image(detected_img_pil_1, caption='Hasil Deteksi dengan Model 1', use_column_width=True)
+    st.image(detected_img_pil_2, caption='Hasil Deteksi dengan Model 2', use_column_width=True)
